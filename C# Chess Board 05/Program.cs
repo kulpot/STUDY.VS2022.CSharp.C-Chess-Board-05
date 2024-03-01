ece8aee60b4a337b2d982278780705c50ea7e7f9
Kulpot <kulpotwahu@gmail.com>
2024-03-01 20:13:20 +0800

    Add project files.

---
+++ C# Chess Board 05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Chess_Board_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
